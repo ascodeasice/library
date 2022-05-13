cadbbebef306091fdeb68af6a816e0d7447cf5ff
ascodeasice <ab0988956087@gmail.com>
2022-05-13 17:43:46 +0800

    Finish the form of adding book, and container of cards. 1.The form validation is done. 2. The data of the pop up form can now be gotten in js file. 3. The functions of buttons are done(adding cards, cancel) 4. most of css are done.
    the rest part is using the data to create cards with info.

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 let myLibrary=[];
+const newBookBtn= document.getElementById("newBookBtn");
+const bookContainer=document.getElementById("bookContainer");
+const form = document.getElementById("popUpForm");
+const submitBtn= document.getElementById("submitBtn");
+const titleInput=document.getElementById("title")
+const authorInput=document.getElementById("author")
+const pagesInput=document.getElementById("pages");
+const readOptions=document.getElementsByName("read");
+const cancelBtn=document.getElementById("cancelBtn");
+
 
 function Book(title, author,pages,read){
     this.title=title;
@@ -10,26 +20,41 @@ function Book(title, author,pages,read){
 function addBookToLibrary(title, author,pages,read){
     let book= new Book(title, author,pages,read);
     myLibrary.push(book);
+    const card=document.createElement("div");
+    card.classList.add("card");
+    bookContainer.appendChild(card);
 }
 
 function displayLibrary(){
-    //TODO change it to 
-    myLibrary.forEach(book=>console.log(book.title,book.author,book.pages,book.read))
 }
 
-function getCard(book){
-    const card=document.createElement("div");
+function hideForm(){
+    titleInput.value="";
+    authorInput.value="";
+    pagesInput.value=0;
+    form.style.visibility="hidden";
 }
-
-const newBookBtn= document.getElementById("newBookBtn");
-const bookContainer=document.getElementById("bookContainer");
 
 newBookBtn.addEventListener("click",function(){
-    //pop up form
-    //addBookToLibrary(data from the form);
-    //displayLibrary();
+    form.style.visibility="visible";
 })
 
-addBookToLibrary("a","b",1,false);
-addBookToLibrary("a","b",1,true);
-displayLibrary();
+submitBtn.addEventListener("click",function(){
+    let title=titleInput.value;
+    let author=authorInput.value;
+    let pages=pagesInput.value;
+    let read="no";
+    for(option in readOptions){
+        if(option.checked)
+            read=option.value;
+    }
+    if(title!=""&&author!=""){
+        addBookToLibrary(title,author,pages,read);
+        hideForm();
+    }
+    displayLibrary();
+})
+
+cancelBtn.addEventListener("click",function(){
+    hideForm();
+})
