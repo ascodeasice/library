86da3af6546220777dc190d5e144b7cd43501e3c
ascodeasice <ab0988956087@gmail.com>
2022-05-14 17:45:08 +0800

    Finish the project The button of changing reading status is done. Some color design is also changed.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const authorInput=document.getElementById("author")
 const pagesInput=document.getElementById("pages");
 const readOptions=document.getElementsByName("read");
 const cancelBtn=document.getElementById("cancelBtn");
-
+const readBtns=document.getElementsByClassName("readBtn");
 
 function Book(title, author,pages,read){
     this.title=title;
@@ -20,18 +20,64 @@ function Book(title, author,pages,read){
 function addBookToLibrary(title, author,pages,read){
     let book= new Book(title, author,pages,read);
     myLibrary.push(book);
-    const card=document.createElement("div");
-    card.classList.add("card");
-    bookContainer.appendChild(card);
+}
+
+function createText(text){
+    const p=document.createElement("p");
+    p.classList.add("cardText");
+    p.innerText=text;
+    return p;
 }
 
 function displayLibrary(){
+    bookContainer.innerText="";
+    for(let i=0;i<myLibrary.length;i++){
+        const card=document.createElement("div");
+        card.classList.add("card");
+
+        //info of the card
+        card.appendChild(createText("Title: "+myLibrary[i].title))
+        card.appendChild(createText("Author: "+myLibrary[i].author))
+        card.appendChild(createText("Pages: "+myLibrary[i].pages))
+        card.appendChild(createText("Has read: "+myLibrary[i].read))
+
+        //the button that change read status
+        const readBtn=document.createElement("button");
+        readBtn.classList.add("readBtn");
+        readBtn.id="readBtn"+String(i);
+        if(myLibrary[i].read=="yes")
+            readBtn.innerText="Not read";
+        else
+            readBtn.innerText="Read";
+
+        readBtn.addEventListener("click",function(){
+            let index=readBtn.id.slice(-1);//last char of string
+            myLibrary[index].read=(myLibrary[index].read=="yes"?"no":"yes");
+            displayLibrary();
+        })
+    
+        card.appendChild(readBtn)
+        card.id="card"+ String(i);
+
+        bookContainer.appendChild(card);
+    }
+
+    // for(let i=0;i<readBtns.length;i++){
+    //     btn=readBtns[i];
+    //     btn.addEventListener("click",function(){
+    //         let index=btn.id.slice(-1);//last char of string
+    //         console.log(index);
+    //         myLibrary[index].read=(myLibrary[index].read=="yes"?"no":"yes");
+    //         displayLibrary();
+    //     })
+    // }
+    
 }
 
 function hideForm(){
     titleInput.value="";
     authorInput.value="";
-    pagesInput.value=0;
+    pagesInput.value=1;
     form.style.visibility="hidden";
 }
 
@@ -44,7 +90,7 @@ submitBtn.addEventListener("click",function(){
     let author=authorInput.value;
     let pages=pagesInput.value;
     let read="no";
-    for(option in readOptions){
+    for(option of readOptions){
         if(option.checked)
             read=option.value;
     }
